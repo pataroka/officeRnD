b3ec5a7a1d45d5c5230d97ecee563d1d476b0cd7
pataroka <pataroque@gmail.com>
2019-09-21 14:45:33 +0300

    Initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    AgGridModule.withComponents([]),
     AppRoutingModule
   ],
   providers: [],
